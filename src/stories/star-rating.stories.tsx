5dc3533ed0d36ef12a8b05bec74440b741ecdb1f
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2025-05-01 19:22:46 +0100

    feat: create a reusable star-rating component

---
+++ src/stories/star-rating.stories.tsx
@@ -0,0 +1,55 @@
+import { ComponentProps } from 'react';
+
+import { Meta, StoryObj } from '@storybook/react';
+
+import StarRating from '@/features/product/components/star-rating';
+
+type StoryProps = ComponentProps<typeof StarRating>;
+
+const meta: Meta<StoryProps> = {
+  component: StarRating,
+  title: 'Components/StarRating',
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    max: {
+      control: {
+        type: 'number',
+      },
+    },
+    initial: {
+      control: {
+        type: 'number',
+      },
+    },
+    locked: {
+      options: [true, false],
+      control: {
+        type: 'select',
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+export const Input: Story = {
+  args: {
+    max: 10,
+    initial: 6.9,
+  },
+  render: (args) => <StarRating {...args} />,
+};
+
+export const Descriptive: Story = {
+  args: {
+    max: 10,
+    initial: 8.2,
+    locked: true,
+  },
+  render: (args) => <StarRating {...args} />,
+};
