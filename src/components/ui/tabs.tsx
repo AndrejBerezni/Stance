be11cd68c8b9c46a452cbd356dfdfca98965848a
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2025-05-20 11:01:50 +0100

    refactor(tabs): add unique id and handle rendering panels differently

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,55 +1,68 @@
 'use client';
 
-import { useState } from 'react';
+import { useId, useState } from 'react';
 
 import { cn } from '@/lib/utils/cn';
 
 export interface Tab {
+  value: string;
   label: string;
-  content: React.ReactNode;
+  panel: React.ReactNode;
 }
 
 export interface TabsProps {
   tabs: Tab[];
+  defaultValue?: string;
 }
 
-export default function Tabs({ tabs }: TabsProps) {
-  const [activeIndex, setActiveIndex] = useState<number>(0);
+export default function Tabs({ tabs, defaultValue }: TabsProps) {
+  const tabsId = useId(); // there might be more tab components on the screen, so we need unique value for aria attributes
+  const [activeTab, setActiveTab] = useState<string>(
+    defaultValue ?? tabs[0].value
+  );
   return (
     <div>
       <div
         role="tablist"
         className="no-scrollbar flex overflow-x-scroll border-b-[1px]"
       >
-        {tabs.map((tab, index) => (
-          <button
-            type="button"
-            key={index}
-            role="tab"
-            aria-selected={activeIndex === index}
-            aria-controls={`tab-panel-${index}`}
-            id={`tab-${index}`}
-            onClick={() => setActiveIndex(index)}
-            className={cn(
-              'border-b-[1px] px-2 pt-0 pb-3 hover:cursor-pointer',
-              {
-                'text-primary border-b-primary': activeIndex === index,
-                'text-ink-600 border-b-transparent': activeIndex !== index,
-              }
-            )}
-          >
-            {tab.label}
-          </button>
-        ))}
+        {tabs.map(({ label, value }) => {
+          const isActive = activeTab === value;
+          return (
+            <button
+              id={`${tabsId}-tab-${value}`}
+              key={value}
+              type="button"
+              role="tab"
+              aria-selected={isActive}
+              aria-controls={`${tabsId}-tab-panel-${value}`}
+              onClick={() => setActiveTab(value)}
+              className={cn(
+                'border-b-[1px] px-2 pt-0 pb-3 hover:cursor-pointer',
+                {
+                  'text-primary border-b-primary': isActive,
+                  'text-ink-600 border-b-transparent': !isActive,
+                }
+              )}
+            >
+              {label}
+            </button>
+          );
+        })}
       </div>
-
-      <div
-        role="tabpanel"
-        id={`tab-panel-${activeIndex}`}
-        aria-labelledby={`tab-${activeIndex}`}
-        className="py-4"
-      >
-        {tabs[activeIndex].content}
+      <div>
+        {tabs.map(({ panel, value }) => (
+          <div
+            id={`${tabsId}-tab-panel-${value}`}
+            key={value}
+            role="tabpanel"
+            aria-labelledby={`${tabsId}-tab-${value}`}
+            className="py-4"
+            hidden={value !== activeTab}
+          >
+            {panel}
+          </div>
+        ))}
       </div>
     </div>
   );
