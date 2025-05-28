f284ba8fba37988e729d116acca743307d400187
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2025-05-28 18:17:38 +0100

    feat(cart): create cart-empty component

--- src/features/cart/components/cart-empty.tsx
+++ src/features/cart/components/cart-empty.tsx
@@ -1,3 +1,31 @@
+import { ShoppingCart, MoveRight } from 'lucide-react';
+import Link from 'next/link';
+import { useLocale, useTranslations } from 'next-intl';
+
+import buttonVariants from '@/components/ui/button/styles';
+import IconContainer from '@/components/ui/icon-container';
+import { cn } from '@/lib/utils/cn';
+
 export default function CartEmpty() {
-  return <div>EMPTY</div>;
+  const translate = useTranslations('cart');
+  const locale = useLocale();
+
+  return (
+    <article className="flex min-h-[40vh] flex-col items-center justify-center gap-5 text-center">
+      <IconContainer>
+        <ShoppingCart size={20} />
+      </IconContainer>
+      <div>
+        <h2 className="mb-2 text-xl font-medium">{translate('emptyCart')}</h2>
+        <p>{translate('exploreCall')}</p>
+      </div>
+      <Link
+        href={`/${locale}/catalogue`}
+        className={cn(buttonVariants({ variant: 'primary', size: 'md' }))}
+      >
+        {translate('explore')}
+        <MoveRight size={18} />
+      </Link>
+    </article>
+  );
 }
