b002c47601ce2cdd86ef702c9579044c752ab013
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2025-06-13 23:11:21 +0100

    fix(ci): resolve conversations from merge request #20

--- cypress/e2e/browse-products.cy.ts
+++ cypress/e2e/browse-products.cy.ts
@@ -1,8 +1,6 @@
-const bypassToken = Cypress.env('VERCEL_BYPASS_TOKEN');
-
 describe('Visit page and browse product catalogue', () => {
   beforeEach(() => {
-    cy.visit(`/?x-vercel-protection-bypass=${bypassToken}`);
+    cy.visit(`/?x-vercel-protection-bypass=${process.env.VERCEL_BYPASS_TOKEN}`);
     // Close 'under construction' info panel
     cy.get('[data-testid="close-under-construction"]').click();
     // Switch language to English
